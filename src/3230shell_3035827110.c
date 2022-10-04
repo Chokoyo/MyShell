a7661b1a60fd9357d5d0d0688f2136220c3036b6
Gu Zhuangcheng <zcgu@academy21.cs.hku.hk>
2022-10-05 00:01:38 +0800

    add 3230shell_3035827110.c to src/

---
+++ src/3230shell_3035827110.c
@@ -0,0 +1,12 @@
+// Student name and No.: Gu Zhuangcheng, 3035827110
+// Development platform: academy11 server, linux
+// Remark – describe how much you have completed; whether you have implemented the bonus part.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
