c23fe6278217fecb094872122edc0cf7b27a0c11
Chokoyo <40918450+Chokoyo@users.noreply.github.com>
2022-10-04 17:02:31 +0000

    add prompt and stdin

--- src/3230shell_3035827110.c
+++ src/3230shell_3035827110.c
@@ -1,12 +1,20 @@
 // Student name and No.: Gu Zhuangcheng, 3035827110
-// Development platform: academy11 server, linux
+// Development platform: c3230-m1-ubuntu docker image
 // Remark – describe how much you have completed; whether you have implemented the bonus part.
 
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+int main(int argc, char *argv[]) {
+   
+   while(1) {
+        printf("$$ 3230shell ## ");
+        char *instr = NULL;
+        size_t len = 0;
+        getline(&instr, &len, stdin);
+        
+
+        
+
+    }
 }
 
